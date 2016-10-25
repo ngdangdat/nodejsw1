a9a9c1aaa92348a34e886fc71d09219c1953789f
Dat Nguyen <ngdangdat09@gmail.com>
2016-10-25 07:24:06 +0700

    67 (84/220)

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,23 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
-var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
+var Ractive = require('ractive');
+var tap = require('gulp-tap');
+
+gulp.task('templates', function(){
+	gulp.src('./tpl/**/*.html')
+	.pipe(tap(function(file, t) {
+		var precompiled = Ractive.parse(file.contents.toString());
+		precompiled = JSON.stringify(precompiled);
+		file.contents = new Buffer('module.exports = ' + precompiled);
+	}))
+	.pipe(rename(function(path) {
+		path.extname = '.js';
+	}))
+	.pipe(gulp.dest('./tpl'))
+});
 
 gulp.task('css', function() {
 	gulp.src('./less/style.less')
@@ -22,12 +37,12 @@ gulp.task('watchers', function() {
 });
 
 gulp.task('js', function(){
-	gulp.src('./js/*.js')
-	.pipe(concat('scripts.js'))
+	gulp.src('./js/app.js')
+	.pipe(browserify())
 	.pipe(gulp.dest('./static/js'))
 	.pipe(uglify())
 	.pipe(rename({suffix: '.min'}))
 	.pipe(gulp.dest('./static/js'))
 });
 
-gulp.task('default', ['css', 'watchers']);
+gulp.task('default', ['css', 'watchers', 'js', 'templates']);
---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+define('modules/ajax', [], function() {
+
+});
+
+define('modules/router', [], function() {
+
+});
+
+require(["modules/ajax", "modules/router"], function(ajax, router) {
+	//..logic
+});
+define("main", function(){});
